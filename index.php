0f9577a062eddac3f8b90caedadc58477516c378
Nicolas Perrenoud <nicolas@ohf-lesvos.org>
2021-12-15 16:01:59 +0200

    Fixed layout

--- index.php
+++ index.php
@@ -45,84 +45,81 @@ $dotenv->safeLoad();
                 ?>
                 <h2 class="display-6"><?= $site->desc ?></h2>
 
-                <div class="row row-cols-1 row-cols-md-2 g-4">
-                    <div class="col">
+                <div class="row row-cols-1 row-cols-md-2 g-4 mb-4">
 
-                        <!-- WAN -->
-                        <?php
-                        $wan = collect($site->health)->filter(fn ($h) => $h->subsystem == 'wan')->first();
-                        ?>
-                        <?php if ($wan !== null && $wan->status != 'unknown') : ?>
-                            <div class="card mb-4">
+                    <!-- WAN -->
+                    <?php
+                    $wan = collect($site->health)->filter(fn ($h) => $h->subsystem == 'wan')->first();
+                    ?>
+                    <?php if ($wan !== null && $wan->status != 'unknown') : ?>
+                        <div class="col">
+                            <div class="card">
                                 <div class="card-header d-flex justify-content-between">
                                     <span><i class="bi-modem"></i> WAN</span>
                                     <strong class="<?= $wan->status == 'ok' ? 'text-success' : 'text-danger' ?>"><?= strtoupper($wan->status) ?></strong>
                                 </div>
-                                <ul class="list-group">
+                                <ul class="list-group list-group-flush">
                                     <li class="list-group-item"><strong>Gateways:</strong> <?= $wan->num_gw ?>/<?= $wan->num_adopted ?></li>
                                     <li class="list-group-item"><strong>WAN IP:</strong> <?= $wan->wan_ip ?></li>
                                     <li class="list-group-item"><strong>Clients:</strong> <?= $wan->num_sta ?></li>
                                     <li class="list-group-item"><strong>Current traffic:</strong> <?= HumanReadableFileSize::getHumanSize($wan->{'tx_bytes-r'}) ?>/<?= HumanReadableFileSize::getHumanSize($wan->{'rx_bytes-r'}) ?> (up/down)</li>
                                 </ul>
                             </div>
-                        <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
 
-                    </div>
-                    <div class="col">
-
-                        <!-- WWW -->
-                        <?php
-                        $www = collect($site->health)->filter(fn ($h) => $h->subsystem == 'www')->first();
-                        ?>
-                        <?php if ($www !== null && $www->status != 'unknown') : ?>
-                            <div class="card mb-4">
+                    <!-- WWW -->
+                    <?php
+                    $www = collect($site->health)->filter(fn ($h) => $h->subsystem == 'www')->first();
+                    ?>
+                    <?php if ($www !== null && $www->status != 'unknown') : ?>
+                        <div class="col">
+                            <div class="card">
                                 <div class="card-header d-flex justify-content-between">
                                     <span><i class="bi-globe"></i> WWW</span>
                                     <strong class="<?= $www->status == 'ok' ? 'text-success' : 'text-danger' ?>"><?= strtoupper($www->status) ?></strong>
                                 </div>
-                                <ul class="list-group">
+                                <ul class="list-group list-group-flush">
                                     <li class="list-group-item"><strong>Latency:</strong> <?= $www->latency ?></li>
                                     <li class="list-group-item"><strong>Uptime:</strong> <?= CarbonInterval::seconds($www->uptime)->cascade()->forHumans() ?></li>
                                     <li class="list-group-item"><strong>Current traffic:</strong> <?= HumanReadableFileSize::getHumanSize($www->{'tx_bytes-r'}) ?>/<?= HumanReadableFileSize::getHumanSize($www->{'rx_bytes-r'}) ?> (up/down)</li>
                                 </ul>
                             </div>
-                        <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
 
-                    </div>
-                    <div class="col">
-
-                        <!-- WLAN -->
-                        <?php
-                        $wlan = collect($site->health)->filter(fn ($h) => $h->subsystem == 'wlan')->first();
-                        ?>
-                        <?php if ($wlan !== null) : ?>
-                            <div class="card mb-4">
+                    <!-- WLAN -->
+                    <?php
+                    $wlan = collect($site->health)->filter(fn ($h) => $h->subsystem == 'wlan')->first();
+                    ?>
+                    <?php if ($wlan !== null) : ?>
+                        <div class="col">
+                            <div class="card">
                                 <div class="card-header d-flex justify-content-between">
                                     <span><i class="bi-wifi"></i> WLAN</span>
                                     <strong class="<?= $wlan->status == 'ok' ? 'text-success' : 'text-danger' ?>"><?= strtoupper($wlan->status) ?></strong>
                                 </div>
-                                <ul class="list-group">
+                                <ul class="list-group list-group-flush">
                                     <li class="list-group-item"><strong>Access points:</strong> <?= $wlan->num_ap ?>/<?= $wlan->num_adopted ?></li>
                                     <li class="list-group-item"><strong>Users:</strong> <?= $wlan->num_user ?></li>
                                     <li class="list-group-item"><strong>Guests:</strong> <?= $wlan->num_guest ?></li>
                                 </ul>
                             </div>
-                        <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
 
-                    </div>
-                    <div class="col">
-
-                        <!-- LAN -->
-                        <?php
-                        $lan = collect($site->health)->filter(fn ($h) => $h->subsystem == 'lan')->first();
-                        ?>
-                        <?php if ($lan !== null && $lan->status != 'unknown') : ?>
-                            <div class="card mb-4">
+                    <!-- LAN -->
+                    <?php
+                    $lan = collect($site->health)->filter(fn ($h) => $h->subsystem == 'lan')->first();
+                    ?>
+                    <?php if ($lan !== null && $lan->status != 'unknown') : ?>
+                        <div class="col">
+                            <div class="card">
                                 <div class="card-header d-flex justify-content-between">
                                     <span><i class="bi-ethernet"></i> LAN</span>
                                     <strong class="<?= $lan->status == 'ok' ? 'text-success' : 'text-danger' ?>"><?= strtoupper($lan->status) ?></strong>
                                 </div>
-                                <ul class="list-group">
+                                <ul class="list-group list-group-flush">
                                     <li class="list-group-item"><strong>Switches:</strong> <?= $lan->num_sw ?>/<?= $lan->num_adopted ?></li>
                                     <?php if (isset($lan->lan_ip)) : ?>
                                         <li class="list-group-item"><strong>LAN IP:</strong> <?= $lan->lan_ip ?></li>
@@ -132,39 +129,39 @@ $dotenv->safeLoad();
                                     <li class="list-group-item"><strong>Current traffic:</strong> <?= HumanReadableFileSize::getHumanSize($lan->{'tx_bytes-r'}) ?>/<?= HumanReadableFileSize::getHumanSize($lan->{'rx_bytes-r'}) ?> (up/down)</li>
                                 </ul>
                             </div>
-                        <?php endif; ?>
-
-                    </div>
+                        </div>
+                    <?php endif; ?>
                 </div>
 
                 <?php
                 $start_date = (new Carbon())->subMonths(12)->getTimestampMs();
                 $results = $unifi_connection->stat_monthly_site($start_date);
                 ?>
-                <table class="table table-striped table-bordered">
-                    <caption>Monthly statistics</caption>
-                    <thead>
-                        <tr>
-                            <th>Month</th>
-                            <th class="text-end">WAN transmitted</th>
-                            <th class="text-end">WAN received</th>
-                            <th class="text-end">WLAN Traffic</th>
-                            <th class="text-end">WLAN clients</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($results as $record) : ?>
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <caption>Monthly statistics</caption>
+                        <thead>
                             <tr>
-                                <td><?= Carbon::createFromTimestampMs($record->time)->format('M Y') ?></td>
-                                <td class="text-end"><?= isset($record->{'wan-tx_bytes'}) ? HumanReadableFileSize::getHumanSize($record->{'wan-tx_bytes'}) : '' ?></td>
-                                <td class="text-end"><?= isset($record->{'wan-rx_bytes'}) ? HumanReadableFileSize::getHumanSize($record->{'wan-rx_bytes'}) : '' ?></td>
-                                <td class="text-end"><?= HumanReadableFileSize::getHumanSize($record->wlan_bytes) ?></td>
-                                <td class="text-end"><?= $record->{'wlan-num_sta'} ?></td>
+                                <th>Month</th>
+                                <th class="text-end">WAN transmitted</th>
+                                <th class="text-end">WAN received</th>
+                                <th class="text-end">WLAN Traffic</th>
+                                <th class="text-end">WLAN clients</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-<hr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($results as $record) : ?>
+                                <tr>
+                                    <td><?= Carbon::createFromTimestampMs($record->time)->format('M Y') ?></td>
+                                    <td class="text-end"><?= isset($record->{'wan-tx_bytes'}) ? HumanReadableFileSize::getHumanSize($record->{'wan-tx_bytes'}) : '' ?></td>
+                                    <td class="text-end"><?= isset($record->{'wan-rx_bytes'}) ? HumanReadableFileSize::getHumanSize($record->{'wan-rx_bytes'}) : '' ?></td>
+                                    <td class="text-end"><?= HumanReadableFileSize::getHumanSize($record->wlan_bytes) ?></td>
+                                    <td class="text-end"><?= $record->{'wlan-num_sta'} ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             <?php endforeach; ?>
             <?php
             $unifi_connection->logout();
