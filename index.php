006bd8bc241265e641d43e76ce33b907a446fca9
Nicolas Perrenoud <nicolas@ohf-lesvos.org>
2022-01-18 15:29:33 +0200

    Added list of devices

--- index.php
+++ index.php
@@ -162,6 +162,51 @@ $dotenv->safeLoad();
                         </tbody>
                     </table>
                 </div>
+
+                <?php
+                    $devices = $unifi_connection->list_devices();
+                ?>
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <caption>Devices</caption>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>IP</th>
+                                <th>MAC</th>
+                                <th>Model</th>
+                                <th>Type</th>
+                                <th>Version</th>
+                                <th>Uptime</th>
+                                <th>Satisfaction</th>
+                                <th>Transmitted</th>
+                                <th>Received</th>
+                                <th># Stations</th>
+                                <th>Adopted</th>
+                                <th>Last seen</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach (collect($devices)->sortBy('name') as $record) : ?>
+                                <tr>
+                                    <td><?= $record->{'name'} ?></td>
+                                    <td><?= $record->{'ip'} ?></td>
+                                    <td><?= $record->{'mac'} ?></td>
+                                    <td><?= $record->{'model'} ?></td>
+                                    <td><?= $record->{'type'} ?></td>
+                                    <td><?= $record->{'version'} ?></td>
+                                    <td><?= CarbonInterval::seconds($record->{'uptime'})->cascade()->forHumans() ?></td>
+                                    <td><?= isset($record->{'satisfaction'}) && $record->{'satisfaction'} >= 0 ? $record->{'satisfaction'} : '-' ?></td>
+                                    <td><?= HumanReadableFileSize::getHumanSize($record->{'tx_bytes'}) ?></td>
+                                    <td><?= HumanReadableFileSize::getHumanSize($record->{'rx_bytes'}) ?></td>
+                                    <td><?= $record->{'num_sta'} ?></td>
+                                    <td><?= $record->{'adopted'} ? 'Yes' : 'No' ?></td>
+                                    <td><?= Carbon::createFromTimestamp($record->{'last_seen'})->isoFormat('LLL') ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             <?php endforeach; ?>
             <?php
             $unifi_connection->logout();
