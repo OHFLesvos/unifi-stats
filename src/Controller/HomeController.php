fc6adad1666ed81498b725ea336108d3ccd745ce
Nicolas Perrenoud <mail@nicu.ch>
2022-04-27 23:48:23 +0300

    alarms on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,14 @@ class HomeController
         return Twig::fromRequest($request)->render($response, 'home.html', [
             'controller_url' => $controller_url,
             'controller' => $unifi_connection->stat_sysinfo()[0],
-            'sites' => collect($unifi_connection->list_sites())->sortBy('name'),
+            'sites' => collect($unifi_connection->list_sites())->sortBy('name')->map(function($site) use ($unifi_connection) {
+                $unifi_connection->set_site($site->name);
+                return [
+                    'name' => $site->name,
+                    'desc' => $site->desc,
+                    'alarm_count' => $unifi_connection->count_alarms(false)[0]->count,
+                ];
+            }),
         ]);
     }
 }
