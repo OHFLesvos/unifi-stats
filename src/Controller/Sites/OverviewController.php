4df0ecb1d81689c72a34fedbc60b8054b28ac622
Nicolas Perrenoud <mail@nicu.ch>
2022-04-27 23:06:38 +0300

    overview refactoring

--- src/Controller/Sites/OverviewController.php
+++ src/Controller/Sites/OverviewController.php
@@ -2,28 +2,24 @@
 
 namespace OHF\UnifiStats\Controller\Sites;
 
-use Slim\Psr7\Response;
-use Slim\Views\Twig;
-
-class OverviewController
+class OverviewController extends BaseSitesController
 {
-    public function __invoke(Response $response, string $site, \UniFi_API\Client $unifi_connection, Twig $twig): Response
+    protected function template(): string
     {
-        $unifi_connection->set_site($site);
+        return 'sites/overview.html';
+    }
 
+    protected function data(\UniFi_API\Client $unifi_connection, $site): array
+    {
         $site_stats = collect($unifi_connection->stat_sites())->firstWhere('name', $site);
 
-        return $twig->render($response, 'sites/overview.html', [
-            'site' => [
-                'name' => $site_stats->name,
-                'desc' => $site_stats->desc,
-                'wan' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'wan')->first(),
-                'www' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'www')->first(),
-                'wlan' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'wlan')->first(),
-                'lan' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'lan')->first(),
-                'vpn' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'vpn')->first(),
-                'alarm_count' => $unifi_connection->count_alarms(false)[0]->count,
-            ],
-        ]);
+        return [
+            'wan' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'wan')->first(),
+            'www' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'www')->first(),
+            'wlan' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'wlan')->first(),
+            'lan' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'lan')->first(),
+            'vpn' => collect($site_stats->health)->filter(fn ($h) => $h->subsystem == 'vpn')->first(),
+            'alarm_count' => $unifi_connection->count_alarms(false)[0]->count,
+        ];
     }
 }
