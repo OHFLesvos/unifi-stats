22622eaa1f41d7608a31a6f35831b3ce46317b42
Nicolas Perrenoud <mail@nicu.ch>
2022-04-28 08:12:43 +0300

    Error logging

--- src/Middleware/UnifiConnectionMiddleware.php
+++ src/Middleware/UnifiConnectionMiddleware.php
@@ -3,6 +3,7 @@
 namespace OHF\UnifiStats\Middleware;
 
 use Fig\Http\Message\StatusCodeInterface;
+use Monolog\Logger;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
@@ -10,7 +11,7 @@ use Slim\Views\Twig;
 
 class UnifiConnectionMiddleware
 {
-    public function __construct(private Twig $twig)
+    public function __construct(private Twig $twig, private Logger $logger)
     {
     }
 
@@ -23,6 +24,9 @@ class UnifiConnectionMiddleware
         $unifi_connection = new \UniFi_API\Client($controller_user, $controller_password, $controller_url, null, null, false);
         $login_result = @$unifi_connection->login();
         if ($login_result !== true) {
+            $this->logger->error("Unable to login to Unifi controller $controller_url with user $controller_user.", [
+                'return_code' => $login_result,
+            ]);
             return $this->twig->render(new Response(StatusCodeInterface::STATUS_BAD_REQUEST), 'errors/connection-error.html', [
                 'controller_url' => $controller_url,
                 'result' => $login_result,
